d8cca8e5c46c66bedc1589e1a11ace54e6c9ab19
Vanessa <110750471+nattdev@users.noreply.github.com>
2023-12-23 20:54:52 -0500

    Add FooterBar component

--- src/components/MarkdownPreviewer.jsx
+++ src/components/MarkdownPreviewer.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import Editor from "./Editor"
 import Previewer from "./Previewer"
+import FooterBar from "./FooterBar";
 import { marked } from "marked";
+import '../styles/markdownPreviewer.css';
+
 
 function MarkdownPreviewer() {
 
@@ -21,10 +24,11 @@ function MarkdownPreviewer() {
     const [markedText, setMarkedText] = useState('');
     
     return (
-        <>
+        <div className="markdown-previewer">
             <Editor onChange={handleChange} />
             <Previewer text={markedText} />
-        </>
+            <FooterBar />
+        </div>
     )
 }
 
