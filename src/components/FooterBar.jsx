162f83c3f087f9283d2a2581b610753593a8ec3e
Vanessa <110750471+nattdev@users.noreply.github.com>
2023-12-23 20:54:35 -0500

    Create FooterBar Component

---
+++ src/components/FooterBar.jsx
@@ -0,0 +1,13 @@
+import '../styles/menubar.css'
+
+function FooterBar() {
+    return (
+        <div className="menubar">
+            <ul role="menubar">
+                <li role="menuitem" >Develop by nattdev</li>
+            </ul>
+        </div>
+    )
+}
+
+export default FooterBar;
